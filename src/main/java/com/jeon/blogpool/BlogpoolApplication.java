8a31d56c68ea7288ff76d4cb1db5a7f0a4e266dc
SmokedDuck1591 <coutinho159123@gmail.com>
2023-04-14 15:57:08 +0900

    project init

---
+++ src/main/java/com/jeon/blogpool/BlogpoolApplication.java
@@ -0,0 +1,13 @@
+package com.jeon.blogpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeon/blogpool/BlogpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeon.blogpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
